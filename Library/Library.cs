20c006244f5bf6ac83ce5a7c4c3afdb4583c837e
IparkuSS <IparkuSS@mail.ru>
2021-04-19 09:18:12 +0300

    Create pr

---
+++ Library/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Library
+    {
+
+        public string Name { set; get; }
+        public string Adress { set; get; }
+        public int YerOpen { set; get; }
+
+    }
+}
